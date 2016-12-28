8553ac2302f60c66f1c91e8851569c8116d418fa
Mike Zhang <ubik00@yahoo.com>
2016-12-27 18:05:07 -0600

    stuff.js works now with EventListener

--- app/assets/javascripts/stuff.js
+++ app/assets/javascripts/stuff.js
@@ -1,1 +1,1 @@
-document.getElementById("heading").innerHTML = "No one here";
+document.addEventListener("DOMContentLoaded", function() { document.getElementById("heading").innerHTML = "No one here"; });
